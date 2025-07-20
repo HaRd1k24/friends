c8a78fc8234f750a45277027e874ea01e03be957
hard1k24 <leva.trapeznikov@mail.ru>
2025-07-20 18:58:20 +0300

    master - ref

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ WORKDIR /app
 # Копируем ВСЕ файлы проекта за один раз
 COPY . .
 # Собираем проект (+ указываем конкретное имя JAR в CMD)
-RUN mvn clean package -DskipTests
+RUN mvn clean package
 # Запускаем приложение (используем точное имя JAR из pom.xml)
 CMD ["java", "-jar", "target/friend-0.0.1-SNAPSHOT.jar"]
--- src/main/java/org/api/friend/service/FriendServiceSearch.java
+++ src/main/java/org/api/friend/service/FriendServiceSearch.java
@@ -1,9 +1,7 @@
 package org.api.friend.service;
 
-import jakarta.annotation.PreDestroy;
 import org.api.friend.exception.NotFoundFriendException;
 import org.api.friend.executerProvider.ExecuteServiceProvider;
-import org.api.friend.mapper.FriendMapper;
 import org.api.friend.model.Friend;
 import org.api.friend.repository.RepositoryService;
 import org.slf4j.Logger;
@@ -11,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Service
 public class FriendServiceSearch {
@@ -19,11 +16,9 @@ public class FriendServiceSearch {
 
     ExecutorService executorService = ExecuteServiceProvider.getExecutorService();
     private final RepositoryService repositoryService;
-    private final FriendMapper mapper;
 
-    public FriendServiceSearch(RepositoryService repositoryService, FriendMapper mapper) {
+    public FriendServiceSearch(RepositoryService repositoryService) {
         this.repositoryService = repositoryService;
-        this.mapper = mapper;
     }
 
     public void searchByName(String id) {
